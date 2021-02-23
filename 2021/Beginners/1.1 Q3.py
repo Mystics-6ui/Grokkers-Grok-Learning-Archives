8f54b3c9019c356600a92776f66c459ba1504542
UdonOli <57059457+UdonOli@users.noreply.github.com>
2021-02-23 14:19:48 +1100

    Create 1.1 Q3.py

---
+++ 2021/Beginners/1.1 Q3.py
@@ -0,0 +1,9 @@
+# Change the green string in this line to 'The mouse takes the cheese'
+
+line = 'The mouse takes the cheese'
+
+# Print the verse:
+print(line)
+print(line)
+print('Hi-ho, the derry-o')
+print(line)
