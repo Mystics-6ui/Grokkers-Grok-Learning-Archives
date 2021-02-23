0dffcede23068efff502c9a92ff89cfe5e482574
UdonOli <57059457+UdonOli@users.noreply.github.com>
2021-02-23 13:51:17 +1100

    Create 1.2 Q1.py

---
+++ 2021/Advanced/1.2 Q1.py
@@ -0,0 +1,12 @@
+def is_notable(name):
+  # Write your function here.
+  print('why')
+
+# Write the rest of your program here.
+nickname = input('Type a nickname: ')
+if len(nickname) < 5 or nickname[0] is not 'N':
+  print('That nickname is not so notable!' )
+elif len(nickname) < 5 and nickname[0] is not 'N':
+  print('That nickname is not so notable!' )
+elif len(nickname) >= 5 and nickname[0] is 'N':
+  print('That nickname is notable!' )
