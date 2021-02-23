c490537995d4a30edc335d3a8a4f5fed8f044905
UdonOli <57059457+UdonOli@users.noreply.github.com>
2021-02-23 14:14:22 +1100

    Create 1.2 Q3.py

---
+++ 2021/Advanced/1.2 Q3.py
@@ -0,0 +1,19 @@
+def to_kmh(knots):
+  # Calculate the speed in km/h
+  print('I have the power of not using def on my side')
+
+# Write the rest of your program here
+kn = float(input('Speed (kn): '))
+
+km = 1.852 * float(kn)
+kmR = round(km, 1)
+
+if kmR >= 120:
+  print(round(km, 1), 'km/h - Whoa! Slow down!')
+if kmR >= 100 and kmR <= 119.9:
+  print(round(km, 1), 'km/h - Radical!')
+if kmR >= 60 and kmR <= 99.9 :
+  print(round(km, 1), 'km/h - Nice one.')
+if kmR < 60:
+  print(round(km, 1), 'km/h - Go faster!')
+
